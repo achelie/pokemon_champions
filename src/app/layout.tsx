47143149dc7c36c6063236df5611fdb0d6a3e349
achelie <x771364026@gmail.com>
2026-06-10 22:55:05 +0800

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 
 import "./globals.css";
@@ -40,6 +41,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <main className="pb-24 md:pb-0">{children}</main>
         <Footer />
         <MobileBottomNav />
+        <Analytics />
       </body>
     </html>
   );
